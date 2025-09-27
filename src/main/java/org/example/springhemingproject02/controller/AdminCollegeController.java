66868408307df5abf8d8d485c2f39f47e1fb9782
heming-jd <yhm050227@qq.com>
2025-09-27 09:57:45 +0800

    1

--- src/main/java/org/example/springhemingproject02/controller/AdminCollegeController.java
+++ src/main/java/org/example/springhemingproject02/controller/AdminCollegeController.java
@@ -1,6 +1,7 @@
 package org.example.springhemingproject02.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admincollege/")
 @RequiredArgsConstructor
 public class AdminCollegeController {
+    @PostMapping("addnode")
+    public void addNode() {
+        System.out.println("addnode");
+    }
 }
